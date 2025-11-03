4b783f37acabb685dff0bbd81ab2b591ea2b776c
Eden Schapira <eden.schapira@post.runi.ac.il>
2025-11-03 08:33:37 +0200

    my first code

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,12 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];//receiving the names of the diners
+	    double bill=Double.parseDouble(args[3]); //receiving the bill amount
+		double splitbill=Math.ceil(bill/3);//equally splitting the bill
+		System.out.println("Dear "+name3+", "+name2+
+		                    ", "+name1+": pay "+(int)splitbill+
+						     " Shekels each");   
 	}
 }
