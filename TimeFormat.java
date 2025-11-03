dd2168b19f3bef10ccc161070ea1e7c1f33e91c0
Eden Schapira <eden.schapira@post.runi.ac.il>
2025-11-03 19:24:38 +0200

    time test1

--- TimeFormat.java
+++ TimeFormat.java
@@ -4,7 +4,6 @@ public class TimeFormat {
 		// In Java, the command-line arguments args[0], args[1], ... are represented
 		// each by a string. In this program, the single "hh:mm" input is represented
 		// by the single command-line string argument args[0]. 
-		//   
 		// The following statement handles the hours part of the input.
 		// It concatenates the empty string "" with the leftmost hour-digit. 
 		// It then concatenates the resulting string with the rightmost hour-digit,
@@ -13,5 +12,15 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         // Replace this comment with the rest of your code
+		if(hours==12){
+			System.out.println(hours+":"+args[0].charAt(3)+ args[0].charAt(4)+" PM");
+		} else if (hours>12) {
+			System.out.println((hours-12)+":"+args[0].charAt(3)+ args[0].charAt(4)+" PM");
+		} else{
+			System.out.println(hours+":"+args[0].charAt(3)+ args[0].charAt(4)+" AM");
+		}
+
+
+
 	}
 }
